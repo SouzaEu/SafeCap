3f3caee72f476b019e6e92c19d9ec18961d190ab
SouzaEu <vinnysouzav@gmail.com>
2025-06-08 00:32:06 -0300

    Adiciona anotação de perfil ao WebSecurityConfig para desabilitar a autenticação em ambientes sem banco de dados.

--- src/main/java/br/com/fiap/safecap/config/WebSecurityConfig.java
+++ src/main/java/br/com/fiap/safecap/config/WebSecurityConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+@Profile("!nodb")
 @Configuration
 public class WebSecurityConfig {
 
