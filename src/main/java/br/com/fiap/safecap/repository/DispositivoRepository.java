68766122153b660d0c854351592ded71f47e5a5c
SouzaEu <vinnysouzav@gmail.com>
2025-06-08 00:41:32 -0300

    Adiciona anotações de perfil aos repositórios e serviços de Alerta e Dispositivo para desabilitar a conexão com o banco de dados em ambientes sem banco.

--- src/main/java/br/com/fiap/safecap/repository/DispositivoRepository.java
+++ src/main/java/br/com/fiap/safecap/repository/DispositivoRepository.java
@@ -2,13 +2,17 @@ package br.com.fiap.safecap.repository;
 
 import br.com.fiap.safecap.model.Dispositivo;
 import br.com.fiap.safecap.model.Usuario;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Profile("!nodb")
+@Repository
 public interface DispositivoRepository extends JpaRepository<Dispositivo, Long> {
     List<Dispositivo> findByUsuario(Usuario usuario);
 
