54d4ad463939f0870d6ab2aca9caaca165e3eb44
SouzaEu <vinnysouzav@gmail.com>
2025-06-07 23:54:49 -0300

    Adiciona perfil "nodb" ao serviço de usuário para desabilitar a conexão com o banco de dados em ambientes específicos.

--- src/main/java/br/com/fiap/safecap/service/UsuarioService.java
+++ src/main/java/br/com/fiap/safecap/service/UsuarioService.java
@@ -6,11 +6,13 @@ import br.com.fiap.safecap.repository.UsuarioRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Profile("!nodb")
 public class UsuarioService {
 
     private static final Logger logger = LoggerFactory.getLogger(UsuarioService.class);
